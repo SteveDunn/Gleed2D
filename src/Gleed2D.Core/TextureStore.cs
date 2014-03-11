7420af5f9eb3c3651b968409adb4b629e5015fcc
epochofentropy <epochofentropy@gmail.com>
2014-03-11 02:56:19 -0500

    -- Adding a setting that will globally flag if imported images have premultiplied alpha channels or not. Default value is previous behavior. -- Updating texture loading to modify the alpha channel if configured for non-premultiplied images

--- src/Gleed2D.Core/TextureStore.cs
+++ src/Gleed2D.Core/TextureStore.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic ;
 using System.Reflection ;
 using Microsoft.Xna.Framework.Graphics ;
 using System.IO ;
+using Microsoft.Xna.Framework.Graphics.PackedVector;
+using Microsoft.Xna.Framework;
 
 namespace Gleed2D.Core
 {
@@ -22,15 +24,52 @@ namespace Gleed2D.Core
 		{
 			if( !_textures.ContainsKey( fullPathToFile ) )
 			{
-				var stream = new FileStream( fullPathToFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) ;
-		
-				_textures[ fullPathToFile ] = Texture2D.FromStream( gd, stream ) ;
+				var stream = new FileStream( fullPathToFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite );
+                
+                Texture2D loadedTexture = Texture2D.FromStream(gd, stream);
+
+                _textures[fullPathToFile] = processTexture(loadedTexture);
 				
-				stream.Close( ) ;
+				stream.Close( );
 			}
 
-			return _textures[ fullPathToFile ] ;
+			return _textures[ fullPathToFile ];
 		}
+
+        private Texture2D processTexture(Texture2D texture)
+        {
+            //Some programs such as Photoshop save PNG and other RGBA images without premultiplied alpha
+            if (!Constants.Instance.PremultipliedAlpha)
+            {
+
+                //XNA 4.0+ assumes that all RGBA images have premultiplied alpha channels.
+                //Code snippet borrowed from http://xboxforums.create.msdn.com/forums/p/62320/383015.aspx#485897
+
+                Byte4[] data = new Byte4[texture.Width * texture.Height];
+                texture.GetData<Byte4>(data);
+                for (int i = 0; i < data.Length; i++)
+                {
+                    Vector4 vec = data[i].ToVector4();
+                    float alpha = vec.W / 255.0f;
+                    int a = (int)(vec.W);
+                    int r = (int)(alpha * vec.X);
+                    int g = (int)(alpha * vec.Y);
+                    int b = (int)(alpha * vec.Z);
+                    uint packed = (uint)(
+                        (a << 24) +
+                        (b << 16) +
+                        (g << 8) +
+                        r
+                        );
+
+                    data[i].PackedValue = packed;
+                }
+
+                texture.SetData<Byte4>(data);
+            }
+
+            return texture;
+        }
 
 		public void Clear()
 		{
